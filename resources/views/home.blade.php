be2e90aad1409781e1595a2268f3f1e415c4f0cd
ksahana19 <ksahana@gmail.com>
2020-07-22 21:08:41 +0530

    Initial Commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="/images/saha.jpeg" width="100px" class="rounded-circle">
+        </div>
+    </div>
+</div>
+@endsection
